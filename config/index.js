b17e21ff6162a98603eee44e8c9f71737754bb66
tanggaolin <tanggaolin123@163.com>
2017-08-07 17:29:22 +0800

    fix:shop url

--- config/index.js
+++ config/index.js
@@ -29,7 +29,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
         '/api': {
-            target: 'http://console.localdev.com',
+            target: 'http://shop.localdev.com',
             changeOrigin: true,
             logLevel: 'debug',
             pathRewrite: {
